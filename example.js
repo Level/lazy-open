3bb0ea7a9e172139b92bb9361c341c81e6f43167
Julian Gruber <julian@juliangruber.com>
2016-12-14 09:24:05 +0100

    add example

--- example.js
+++ example.js
@@ -1,2 +1,25 @@
-var level-lazy-open = require('.');
+var lazy = require('./');
+var down = require('memdown');
 
+// a function that returns an open backend
+function factory(cb){
+  var db = down('./db');
+  db.open(function(err){
+    cb(err, db);
+  });
+}
+
+var db = lazy(factory);
+
+// db is closed and won't be opened, unless
+// you do something with it:
+
+db.put('foo', 'bar', function(err){
+  // db is open now
+
+  // you can manually close the db again:
+  db.close(function(err){
+    // the end
+  });
+});
+// db is opening now
